d534b00856921d73600100905ae5472b3634eee7
joaonetocastro <jneto111@gmail.com>
2020-08-10 21:53:35 -0300

    adding feedback on user update

--- src/components/modals/EditProductModal.vue
+++ src/components/modals/EditProductModal.vue
@@ -10,18 +10,42 @@
             placeholder="João
           Fulano"
             outlined
+            :disabled="sendingData"
           />
           <v-text-field
             v-model="product.barcode"
             label="Cód. de barras"
             placeholder="xxxxxxxxxxxx"
             outlined
+            :disabled="sendingData"
           />
+          <v-alert
+            type="success"
+            border="bottom"
+            dense
+            text
+            elevation="5"
+            v-if="success"
+          >
+            Produto atualizado com sucesso
+          </v-alert>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="save()">Salvar</v-btn>
-          <v-btn color="red darken-1" text @click="close()">Cancelar</v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="save()"
+            :disabled="sendingData"
+            >Salvar</v-btn
+          >
+          <v-btn
+            color="red darken-1"
+            text
+            @click="close()"
+            :disabled="sendingData"
+            >Cancelar</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -33,22 +57,29 @@ export default {
   name: "EditProducModal",
   props: ["product", "afterEdit"],
   data: () => ({
-    open: false
+    open: false,
+    success: false,
+    sendingData: false
   }),
   created() {
     this.open = Boolean(this.product);
   },
   methods: {
     async save() {
+      this.sendingData = true;
       try {
         if (this.product) {
           await ProductService.save(this.product);
-          this.afterEdit(this.product);
-          this.close();
+          this.success = true;
+          setTimeout(() => {
+            this.afterEdit(this.product);
+            this.close();
+          }, 800);
         }
       } catch (error) {
         console.error(error);
       }
+      this.sendingData = true;
     },
     close() {
       this.open = false;
